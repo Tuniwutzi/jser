e515ef847937bbe44476728ccfa7e358d2ce3863
Jan Buenker <jan.buenker@grandcentrix.net>
2025-08-17 18:36:56 +0200

    Remove success tag from serialization tests

--- jser/tests/Serialization.cpp
+++ jser/tests/Serialization.cpp
@@ -6,19 +6,19 @@
 
 using Integrals = std::tuple<int8_t, uint8_t, int16_t, uint16_t, int32_t, uint32_t, int64_t, uint64_t>;
 
-TEMPLATE_LIST_TEST_CASE("Serialize integral success", "[serialize][success]", Integrals) {
+TEMPLATE_LIST_TEST_CASE("Serialize integral", "[serialize]", Integrals) {
     static_assert(jser::serialize<TestType>(123) == "123");
     std::string integral = jser::serialize<TestType>(106);
     REQUIRE(integral == "106");
 }
 
-TEST_CASE("Serialize bool success", "[serialize][success]") {
+TEST_CASE("Serialize bool", "[serialize]") {
     static_assert(jser::serialize(true) == "true");
     std::string value = jser::serialize<bool>(false);
     REQUIRE(value == "false");
 }
 
-TEST_CASE("Serialize string success", "[serialize][success]") {
+TEST_CASE("Serialize string", "[serialize]") {
     static_assert(jser::serialize("foo") == "\"foo\"");
     std::string value = jser::serialize("bar");
     REQUIRE(value == "\"bar\"");
